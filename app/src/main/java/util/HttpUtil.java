560d4bd00d1242cdffba40585a48114664f113de
成 <1440491813@qq.com>
2017-06-17 18:48:11 +0800

    遍历省市县三级列表

---
+++ app/src/main/java/util/HttpUtil.java
@@ -0,0 +1,20 @@
+package util;
+
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Created by 成 on 2017/6/17.
+ */
+
+public class HttpUtil {
+    public static void sendOkHttpRequest(String address , okhttp3.Callback callback){
+        OkHttpClient client = new OkHttpClient();
+        Request request  = new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+
+    }
+
+
+}
